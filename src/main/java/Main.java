0cf642fc6f834d29c151c71488441340c0b1de4f
Zalundacat <101882998+Zalundacat@users.noreply.github.com>
2022-06-23 22:59:00 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
@@ -18,7 +17,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,11 +33,11 @@ public class Main {
 
         // CSV --> Json
         List<Employee> listFromCSV = parseCSV(columnMapping, csvFileName);
-        String jsonFromCSV = (String) listToJson(listFromCSV);
+        String jsonFromCSV = listToJson(listFromCSV);
         writeString(jsonFromCSV, csvJsonFileName);
         // XML --> Json
         List<Employee> listFromXML = parseXML(xmlFileName);
-        String jsonFromXML = (String) listToJson(listFromXML);
+        String jsonFromXML = listToJson(listFromXML);
         writeString(jsonFromXML, xmlJsonFileName);
 
     }
@@ -94,12 +92,10 @@ public class Main {
         return null;
     }
 
-    public static <Employee> Object listToJson(List<Employee> list) {
+    public static String listToJson(List<Employee> list) {
         GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
-        Type listType = new TypeToken<List<Employee>>() {
-        }.getType();
-        return gson.toJson(list, listType);
+        Gson gson = builder.setPrettyPrinting().create();
+        return gson.toJson(list);
     }
 
     public static void writeString(String json, String fileName) {
